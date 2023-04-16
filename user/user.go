7c9824baa35bd9f1edcc602c26b4d5288513b483
Erickype <67160282+Erickype@users.noreply.github.com>
2023-04-16 11:02:09 -0500

    User get handler by email address

--- user/user.go
+++ user/user.go
@@ -19,6 +19,18 @@ type CreateResponse struct {
 	Status string `json:"status"`
 }
 
+// Get returns a user based on its email
+//
+//encore:api public method=GET path=/user/:email
+func Get(ctx context.Context, email string) (*User, error) {
+	u := &User{Email: email}
+	err := sqldb.QueryRow(ctx, `
+		SELECT user_id FROM "user"
+		WHERE email = $1
+	`, email).Scan(&u.Id)
+	return u, err
+}
+
 // Create creates a user receiving an email
 //
 //encore:api public method=POST path=/user
